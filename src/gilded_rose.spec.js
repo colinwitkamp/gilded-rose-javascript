166de4e9dfbf491a52365ca71b7ae393e0a525dc
colin <colinwitkamp@gmail.com>
2021-12-20 03:11:42 -0800

    gilded_rose.spec: The *quality* of an item is never negative

--- src/gilded_rose.spec.js
+++ src/gilded_rose.spec.js
@@ -12,6 +12,7 @@ beforeEach(() => {
   items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
   items.push(new Item("Conjured Mana Cake", 3, 6));
   items.push(new Item("Baduweiser Beer", 0, 50));
+  items.push(new Item("Heneken", 1, 1));
 
   items = update_quality(items);
 });
@@ -25,4 +26,8 @@ test("At the end of each day our system lowers both values for every item", () =
 test("Once the *sell_in* days is less then zero, *quality* degrades twice as fast", () => {
   expect(items[6].quality).toBe(48);
   expect(items[6].sell_in).toBe(-1);
+});
+
+test("The *quality* of an item is never negative", () => {
+  expect(items[7].quality).toBeGreaterThanOrEqual(0);
 });
